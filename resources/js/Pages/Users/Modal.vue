450402cc625a0999cbcd8cce53ceeca2a288823b
Luis Parrado <luisprmat@gmail.com>
2025-04-26 22:35:32 -0500

    Smooth Transitions

--- resources/js/Pages/Users/Modal.vue
+++ resources/js/Pages/Users/Modal.vue
@@ -9,16 +9,41 @@ defineProps({
 
 <template>
     <Teleport to="body">
-        <div
-            v-show="show"
-            class="fixed inset-0 flex size-full items-center justify-center"
-        >
-            <div class="fixed inset-0 z-40 flex size-full bg-black/75" />
+        <Transition leave-active-class="duration-300">
             <div
-                class="z-50 w-full max-w-md rounded-lg bg-white p-4 dark:bg-gray-800"
+                v-show="show"
+                class="fixed inset-0 flex size-full items-center justify-center"
             >
-                <slot />
+                <Transition
+                    enter-from-class="opacity-0"
+                    enter-to-class="opacity-100"
+                    enter-active-class="transition duration-300"
+                    leave-from-class="opacity-100"
+                    leave-to-class="opacity-0"
+                    leave-active-class="transition duration-300"
+                >
+                    <div
+                        v-show="show"
+                        class="fixed inset-0 z-40 flex size-full bg-black/75"
+                    />
+                </Transition>
+
+                <Transition
+                    enter-from-class="opacity-0 scale-90"
+                    enter-to-class="opacity-100 scale-100"
+                    enter-active-class="transition duration-300"
+                    leave-from-class="opacity-100 scale-100"
+                    leave-to-class="opacity-0 scale-90"
+                    leave-active-class="transition duration-300"
+                >
+                    <div
+                        v-show="show"
+                        class="z-50 w-full max-w-md rounded-lg bg-white p-4 dark:bg-gray-800"
+                    >
+                        <slot />
+                    </div>
+                </Transition>
             </div>
-        </div>
+        </Transition>
     </Teleport>
 </template>
